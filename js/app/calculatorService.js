75a7181c5943a0de3dc856498a29a991745bb469
Daan Stolp <mail@daanstolp.nl>
2017-03-17 17:52:00 +0100

    Implemented simple add function in the calculatorService

--- js/app/calculatorService.js
+++ js/app/calculatorService.js
@@ -5,8 +5,10 @@
     angular.module('tddcomponent')
            .service('calculatorService', calculatorService);
 
-    function calculatorService() {
-        
+    function calculatorService($q) {
+        this.add = function(n1, n2) {
+            return $q.when(n1 + n2);
+        };
     }
 
 })();
--- js/specs/calculatorService.spec.js
+++ js/specs/calculatorService.spec.js
@@ -1,14 +1,30 @@
-describe('CalculatorService', function() {
+describe('CalculatorService', function () {
 
-    var calculatorService;
+    var $rootScope,
+        calculatorService;
 
     beforeEach(module('tddcomponent'));
-    beforeEach(inject(function (_calculatorService_) {
+    beforeEach(inject(function (_calculatorService_, _$rootScope_) {
+        $rootScope = _$rootScope_;
         calculatorService = _calculatorService_;
     }));
 
-    it('should exist', function() {
+    it('should exist', function () {
         expect(calculatorService).toBeDefined();
     });
+
+    it('should return the result of an addition as a promise', function () {
+        calculatorService.add(1, 2).then(function (result) {
+            expect(result).toBe(3);
+        });
+        $rootScope.$apply();
+    });
+
+    it('should handle negative numbers correctly', function () {
+        calculatorService.add(-3, 2).then(function (result) {
+            expect(result).toBe(-1);
+        });
+        $rootScope.$apply();
+    })
 
 });
